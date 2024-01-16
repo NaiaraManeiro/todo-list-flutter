f6dcce58a38ec1af797be0719283a97f839767ce
nmaneiro <nmaneiro@seidor.es>
2024-01-16 09:09:00 +0100

    Localization and BBDD added

---
+++ lib/helpers/helpers/sql_helper.dart
@@ -0,0 +1,51 @@
+import 'package:sqflite/sqflite.dart' as sql;
+
+
+class SQLHelper {
+  static const _databaseName = "TodoApp.db";
+  static const _databaseVersion = 1;
+
+    static Future<sql.Database> db() async {
+    return sql.openDatabase(
+      _databaseName,
+      version: _databaseVersion,
+      onCreate: (sql.Database database, int version) async {
+        await createTables(database);
+      },
+    );
+  }
+
+  static Future<void> createTables(sql.Database database) async {
+    await database.execute("""CREATE TABLE IF NOT EXISTS users(
+        email TEXT PRIMARY KEY NOT NULL,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL)""");
+    await database.execute("""CREATE TABLE IF NOT EXISTS categoriesU(
+        emailU TEXT NOT NULL,
+        name TEXT NOT NULL,
+        icon TEXT NOT NULL,
+        PRIMARY KEY (emailU, name))""");
+    await database.execute("""CREATE TABLE IF NOT EXISTS notesU(
+        id INTEGER AUTOINCREMENT,
+        emailU TEXT NOT NULL,
+        name TEXT NOT NULL,
+        dateIni TEXT NOT NULL,
+        dateFin TEXT NOT NULL,
+        PRIMARY KEY (id, emailU))""");
+  }
+
+  static Future<void> cleanDatabase() async {
+    try{
+      final database = await db();
+      await database.transaction((txn) async {
+        var batch = txn.batch();
+        batch.delete('users');
+        batch.delete('categoriesU');
+        batch.delete('notesU');
+        await batch.commit();
+      });
+    } catch(error){
+      throw Exception('DbBase.cleanDatabase: $error');
+    }
+  }
+}
